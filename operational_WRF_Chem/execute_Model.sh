266aee118cae7548fe2a83a9ad6a993312b44acf
karafede <karafede@hotmail.com>
2017-09-28 16:46:11 +0400

    Add files via upload

--- operational_WRF_Chem/execute_Model.sh
+++ operational_WRF_Chem/execute_Model.sh
@@ -199,3 +199,48 @@ bsub -I -n 96 -J chem-wrf -o vk.%J.out -e vk.%J.err -q normal mpirun ./wrf.exe
 
 echo "WRF Chem Ends"
 exit
+
+
+######################################  Source Env variables ##########################################################
+export LC_LIBRARY_PATH=/apps/netcdf/installed/lib
+module load gcc/4.9.2
+module load openmpi/1.8.4
+export LC_LIBRARY_PATH=/apps/libpng/libpng-1.4.13/lib:${LD_LIBRARY_PATH}
+source "/home/fkaragulian/WRFV3/wrfchem_env.txt"  
+export PATH=/apps/cdo/cdo-1.7.2/bin:${PATH}
+module load ncview
+export PATH=/apps/ncl/ncl-6.3.0/bin:${PATH}
+export NCARG_ROOT=/apps/ncl/ncl-6.3.0/
+
+#######################################################################################################################
+
+############################################ Move WRF outputs files to CESAM  #############################################################################
+#dir=/research/cesam/AirQuality/WRF_outputs/
+#mkdir -p ${input}/${date} ; cd ${input}/${date}/
+
+
+#mv wrfout_d0* /research/cesam/AirQuality/WRF_outputs/2017092800 
+
+########################################### Post Processing (to run for each output hour ##################################################################
+
+dir=/research/cesam/AirQuality/WRF_outputs/2017092800
+cd ${dir}/
+files=wrfout_d0*
+ echo ${files}
+for i in ${files[@]}; do
+ output=( `echo ${i##*/}|sed 's/wrfout/wrfpost/'`  )
+ #echo file_in is $i 
+ #echo file_out is $output 
+ #ncl 'file_in="'$i'"' 'file_out="./'$output'.nc"' ./wrfpost_dust_20170927_airquality.ncl
+ ncl 'file_in="'$i'"' 'file_out="./'$output'.nc"' /home/fkaragulian/bin/wrfpost_dust_20170927_airquality.ncl
+ done
+
+
+
+########################################## R scripts to generate .TIFF Files ################################################################################
+
+
+
+
+
+
