f49f64212ab7a2a5881db3ded4d36f8897354bc8
karafede <karafede@hotmail.com>
2017-12-17 10:45:18 +0400

    Add files via upload

--- operational_WRF_Chem/submit_WRF_Chem.sh
+++ operational_WRF_Chem/submit_WRF_Chem.sh
@@ -9,13 +9,13 @@ export LSF_BINDIR=/shared/ibm/platform_lsf/9.1/linux2.6-glibc2.3-x86_64/bin
 export LSF_ENVDIR=/shared/ibm/platform_lsf/conf
 cd /disk3/fkaragulian/WRF_UAE/scripts/
 # General queque
-bsub -n 96 -K -R "span[ptile=24]" -W 6:00 -q general -J WRF_Chm -o WRF_Chm.%J.out -e WRF_Chm.%J.err "/disk3/fkaragulian/WRF_UAE/scripts/execute_Model_lsf.sh 1>/home/fkaragulian/log/chemrun.log 2>/home/fkaragulian/log/chem-error.log"
+# bsub -n 96 -K -R "span[ptile=24]" -W 6:00 -q general -J WRF_Chm -o WRF_Chm.%J.out -e WRF_Chm.%J.err "/disk3/fkaragulian/WRF_UAE/scripts/execute_Model_lsf.sh 1>/home/fkaragulian/log/chemrun.log 2>/home/fkaragulian/log/chem-error.log"
 
 # reserved nodes
-#bsub -n 96 -K -U wrf_chem_00 -R "span[ptile=24]" -W 6:00 -q general -J WRF_Chm -o WRF_Chm.%J.out -e WRF_Chm.%J.err "/disk3/fkaragulian/WRF_UAE/scripts/execute_Model_lsf.sh 1>/home/fkaragulian/log/chemrun.log 2>/home/fkaragulian/log/chem-error.log"
+bsub -n 96 -K -U wrf_chem_00z -R "span[ptile=24]" -W 6:00 -q general -J WRF_Chm -o WRF_Chm.%J.out -e WRF_Chm.%J.err "/disk3/fkaragulian/WRF_UAE/scripts/execute_Model_lsf.sh 1>/home/fkaragulian/log/chemrun.log 2>/home/fkaragulian/log/chem-error.log"
 
 #Post-processing (with NCL and R)
-bsub   -W 1:00 -q general -J WRF_Chm_Post -o WRF_Chm_Post.%J.out -e WRF_Chm_Post.%J.err /disk3/fkaragulian/WRF_UAE/scripts/POSTPROCESSING.sh
+ bsub   -W 1:00 -q general -J WRF_Chm_Post -o WRF_Chm_Post.%J.out -e WRF_Chm_Post.%J.err /disk3/fkaragulian/WRF_UAE/scripts/POSTPROCESSING.sh
 # bsub   -W 1:00 -q general -J WRF_Chm_Post -o WRF_Chm_Post.%J.out -e WRF_Chm_Post.%J.err /disk3/fkaragulian/WRF_UAE/scripts/short_execute_Model.sh
 
 
